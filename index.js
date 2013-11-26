9881d854467d7a65d2945fbd02a1659013e61c49
David Tudury <david.tudury@voxer.com>
2013-11-26 06:49:21 -0800

    updated version: 0.0.2

--- index.js
+++ index.js
@@ -1,31 +1,35 @@
-
-var qRequestBrowser = require('./lib/qRequestBrowser');
 var qRequestNode = require('./lib/qRequestNode');
+var qRequestBrowser = require('./lib/qRequestBrowser');
 
 
 function request(options, body, method) {
     if (typeof document === "undefined") {
         return qRequestNode(options, body, method);
     } else {
-        return qRequestNode(options, body, method);
+        return qRequestBrowser(options, body, method);
     }
 }
+
 
 function get(options) {
     return request(options, null, 'GET');
 }
 
+
 function post(options, body) {
     return request(options, body, 'POST');
 }
+
 
 function del(options) {
     return request(options, null, 'DELETE');
 }
 
+
 function put(options, body) {
     return request(options, body, 'PUT');
 }
+
 
 exports.verbose = true;
 exports.secureProtocol = true;
